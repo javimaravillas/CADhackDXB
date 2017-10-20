bef703d7e0bf3fb7cf7c20cdd03a882c78238f3a
Jeff Wentworth <jeff@curvegrid.com>
2017-10-20 16:52:33 +0400

    add simple test

--- test/cardtable.js
+++ test/cardtable.js
@@ -1,16 +1,36 @@
-var SimpleStorage = artifacts.require("./CardTable.sol");
+var CardTable = artifacts.require("./CardTable.sol");
+
+function allGasUsedUp(txn) {
+  // check that given transaction didn't throw an exception by running out of gas
+  var tx = web3.eth.getTransaction(txn.tx);
+  var txr = txn.receipt;
+
+  return txr.gasUsed === tx.gas;
+}
 
 contract('CardTable', function(accounts) {
+  var ct;
 
-  it("...should store the value 89.", function() {
-    return SimpleStorage.deployed().then(function(instance) {
-      simpleStorageInstance = instance;
+  var u = {
+    p0: accounts[0],
+    p1: accounts[1],
+    p2: accounts[2],
+  };
 
-      return simpleStorageInstance.set(89, {from: accounts[0]});
-    }).then(function() {
-      return simpleStorageInstance.get.call();
-    }).then(function(storedData) {
-      assert.equal(storedData, 89, "The value 89 was not stored.");
+  beforeEach(function() {
+    return CardTable.new({from: u.p0})
+    .then(function(instance) {
+      ct = instance;
+    });
+  });
+
+  it("should register three new players", function() {
+    return ct.registerPlayer("Player 0", {from: u.p0})
+    .then(function(txn) {
+      // check that an exception wasn't thrown
+      assert.isNotTrue(allGasUsedUp(txn), "All gas was used up, registerPlayer() threw an exception.");
+
+      return web3.eth.getBalance(contract.address);
     });
   });
 
