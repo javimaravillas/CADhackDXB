093cb84b90626b68130faeaa621103913646e710
Jeff Wentworth <jeff@curvegrid.com>
2017-10-20 18:21:29 +0400

    build out base case test

--- test/cardtable.js
+++ test/cardtable.js
@@ -17,7 +17,7 @@ contract('CardTable', function(accounts) {
     p2: accounts[2],
   };
 
-  var buyInAmount = 10;
+  var buyInAmount = web3.toWei(10, "ether");
 
   beforeEach(function() {
     return CardTable.new({from: u.p0})
@@ -26,7 +26,7 @@ contract('CardTable', function(accounts) {
     });
   });
 
-  it("should register three new players", function() {
+  it("should register three new players, they join a game, and the game starts", function() {
     return contract.registerPlayer("Player 0", {from: u.p0})
     .then(function(txn) {
       // check that an exception wasn't thrown
@@ -48,15 +48,25 @@ contract('CardTable', function(accounts) {
     })
     .then(function(count) {
       assert.equal(count, 3, "Did not register the expected number of players.");
+    
+      return contract.joinGame({from: u.p0, value: buyInAmount})
+    })
+    .then(function(txn) {
+      // check that an exception wasn't thrown
+      assert.isNotTrue(allGasUsedUp(txn), "All gas was used up, joinGame() threw an exception.");
+
+      return contract.joinGame({from: u.p1, value: buyInAmount})
+    })
+    .then(function(txn) {
+      // check that an exception wasn't thrown
+      assert.isNotTrue(allGasUsedUp(txn), "All gas was used up, joinGame() threw an exception.");
+
+      return contract.joinGame({from: u.p2, value: buyInAmount})
+    })
+    .then(function(txn) {
+      // check that an exception wasn't thrown
+      assert.isNotTrue(allGasUsedUp(txn), "All gas was used up, joinGame() threw an exception.");
     });
   });
-
-  // it("three players should join a game and the game should start", function() {
-  //   return contract.joinGame({from: u.p0, value: buyInAmount})
-  //   .then(function(txn) {
-  //     // check that an exception wasn't thrown
-  //     assert.isNotTrue(allGasUsedUp(txn), "All gas was used up, joinGame() threw an exception.");
-  //   });
-  // });
 
 });
