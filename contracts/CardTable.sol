3bcdc58860029df1d1f3da7a2e13599c69269d80
Jeff Wentworth <jeff@curvegrid.com>
2017-10-20 11:26:59 +0400

    fill out joinGame()

--- contracts/CardTable.sol
+++ contracts/CardTable.sol
@@ -43,12 +43,11 @@ contract CardTable {
 
   // events
   event RegisteredPlayer(uint256 playerId, address playerAccount, string name);
-  event WaitingForGame(address playerAccount, uint256 buyInAmount);
-	event ErrorJoiningGame(address playerAccount, uint256 buyInAmount);
-  event ConnectToGame(address playerAccount, uint256 gameId, address nextPeer, address prevPeer);
-  event PaidWinner(address playerAccount, uint256 gameId, uint256 roundNum, uint256 payoutAmount);
-  event PlayerTimeout(address playerAccount, address notifierAccount, uint256 gameId, uint256 roundNum);
-  event GameTimeout(address notifierAccount, uint256 gameId);
+  event WaitingForGame(uint256 gameId, address playerAccount, uint256 buyInAmount);
+  event GameStarting(uint256 gameId, address playerAccount, address nextPeer, address prevPeer);
+  event PaidWinner(uint256 gameId, address playerAccount, uint256 roundNum, uint256 payoutAmount);
+  event PlayerTimeout(uint256 gameId, address playerAccount, address notifierAccount, uint256 roundNum);
+  event GameTimeout( uint256 gameId, address notifierAccount);
   event PayoutTimeout(uint256 gameId, uint256 roundNum, uint256 payoutAmount);
   event GameFinished(uint256 gameId);
 
@@ -108,12 +107,59 @@ contract CardTable {
     return true;
   }
 
+  function getPlayer(address playerAccount) private returns(Player p) {
+    return players[addressToPlayer[playerAccount]];
+  }
+
+  function newGame() private returns(Game g) {
+    Player[] ps;
+    Round[] rs;
+
+    g = Game(0, numPlayers, numRounds, buyInAmount, ps, rs);
+
+    // retrieve id as part of array addition to save on gas
+    uint256 id = games.push(g) - 1;
+    games[id].id = id;
+
+    return g;
+  }
+
   function joinGame() public payable returns(bool success) {
-    require(msg.value >= buyInAmount);
+    require(msg.value == buyInAmount);
     require(playerExists(msg.sender));
 
     // add to nextGame
+    Player memory p = getPlayer(msg.sender);
+    nextGame.players.push(p);
+    WaitingForGame(nextGame.id, msg.sender, buyInAmount);
 
     // if nextGame is full, start the game
+    if (nextGame.players.length == nextGame.numPlayers) {
+
+      // notify the players to connect to their peers
+      for (uint i = 0; i < nextGame.players.length; i++) {
+        address nextPeer;
+        address prevPeer;
+
+        if (i < nextGame.players.length - 1) {
+          nextPeer = nextGame.players[i + 1].account;
+        } else {
+          nextPeer = nextGame.players[0].account;
+        }
+
+        if (i > 0) {
+          prevPeer = nextGame.players[i - 1].account;
+        } else {
+          prevPeer = nextGame.players[nextGame.players.length - 1].account;
+        }
+
+        GameStarting(nextGame.id, nextGame.players[i].account, nextPeer, prevPeer);
+      }
+
+      // reset nextGame
+      nextGame = newGame();
+    }
+
+    return true;
   }
 }
