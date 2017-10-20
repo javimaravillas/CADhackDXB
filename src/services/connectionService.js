f9ad038ec5e3d25484195c21f5a64230c91d5869
Foluso Ogunlana <folusoogunlana@Folusos-MacBook-Air.local>
2017-10-20 17:03:42 +0400

    reformatted tabs

--- src/services/connectionService.js
+++ src/services/connectionService.js
@@ -3,88 +3,88 @@ import Peer from 'peerjs';
 
 const ConnectionService = () =>{
 
-    let peer;
-    const connectedPeers = {};
-    
-    function setup(address, connectedPeersCallback) {
-	return new Promise((resolve, reject) => {
-        peer = new Peer(address, {
-		host: '10.0.212.79',
-		port: 9000,
-		debug: 3,
-		logFunction: function() {
-                    var copy = Array.prototype.slice.call(arguments).join(' ');
-		    console.log(copy);
-                    //$('.log').append(copy + '<br>');		
-		}
-            });
-            peer.on('error', function(err) {
-		console.log(err);
-		reject(err);
-            });
-            peer.on('open', (id) => {
-		console.log('open', {id});
-		resolve(id);
-            });
-	    
-	    // Await connections from others
-        peer.on('connection', (c) => {
-            console.log('connection', c);
-            configureConnection(c, connectedPeersCallback);
-	    });
-	});
+  let peer;
+  const connectedPeers = {};
+
+  function setup(address, connectedPeersCallback) {
+    return new Promise((resolve, reject) => {
+      peer = new Peer(address, {
+        host: '10.0.212.79',
+        port: 9000,
+        debug: 3,
+        logFunction: function() {
+          var copy = Array.prototype.slice.call(arguments).join(' ');
+          console.log(copy);
+          //$('.log').append(copy + '<br>');
+        }
+      });
+      peer.on('error', function(err) {
+        console.log(err);
+        reject(err);
+      });
+      peer.on('open', (id) => {
+        console.log('open', {id});
+        resolve(id);
+      });
+
+      // Await connections from others
+      peer.on('connection', (c) => {
+        console.log('connection', c);
+        configureConnection(c, connectedPeersCallback);
+      });
+    });
+  }
+  function configureConnection(connection, callback) {
+    if(!connectedPeers[connection.peer]) {
+      connection.on('error', (err) => {
+        console.log(err);
+        alert(err);
+      });
+      connection.on('open', () => {
+        console.log("connected to: ", connection.peer);
+        connectedPeers[connection.peer] = connection;
+        connection.on('data', (value) => {
+          console.log('data', JSON.stringify({value}));
+        });
+        if(callback) {
+          callback(connection.peer);
+        }
+      });
     }
-    function configureConnection(connection, callback) {
-        if(!connectedPeers[connection.peer]) {
-            connection.on('error', (err) => {
-                console.log(err);
-                alert(err);
-            });
-            connection.on('open', () => {
-                console.log("connected to: ", connection.peer);
-                connectedPeers[connection.peer] = connection;
-                connection.on('data', (value) => {
-                    console.log('data', JSON.stringify({value}));
-                });
-                if(callback) {
-                    callback(connection.peer);                    
-                }
-            });
-    }
-}
+  }
   function connect(peerId) {
     // Handle a chat connection.
-      return new Promise((resolve, reject) => {
-	  if (!connectedPeers[peerId]) {
-	      console.log("connecting to peer: ", peerId);
-	      var c = peer.connect(peerId, {
-		  label: 'chat',
-		  serialization: 'none',
-		  metadata: {message: 'join game request'}
-	      });
-          configureConnection(c, resolve);
-	  } else {
-	      console.log("already connected!", peerId);
-	  }
-      });
+    return new Promise((resolve, reject) => {
+      if (!connectedPeers[peerId]) {
+        console.log("connecting to peer: ", peerId);
+        var c = peer.connect(peerId, {
+          label: 'chat',
+          serialization: 'none',
+          metadata: {message: 'join game request'}
+        });
+        configureConnection(c, resolve);
+      } else {
+        console.log("already connected!", peerId);
+      }
+    });
   }
 
-    function send(peerId, obj) {
-	const c = connectedPeers[peerId];
-	if (c) {
-	    console.log({c});	    
-	    c.send(obj);	    
-	} else {
-	    alert("You are not connect to: ", peerId);
-	}
+  function send(peerId, obj) {
+    const c = connectedPeers[peerId];
+    if (c) {
+      console.log({c});
+      c.send(obj);
+    } else {
+      alert("You are not connect to: ", peerId);
     }
+  }
 
-    
-    return {
-	setup, 
-	connect,
-	send
-    };
+
+  return {
+    setup,
+    connect,
+    send
+  };
 }
 
 
