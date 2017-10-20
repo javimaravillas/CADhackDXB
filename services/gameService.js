445d6308f6886b6349f47ff524ebf2ec5b7ad5ca
Mikhail <m.dobrokhvalov@gmail.com>
2017-10-20 12:19:35 +0400

    game logic file inited

---
+++ services/gameService.js
@@ -0,0 +1,47 @@
+
+
+
+const GameService = () => {
+
+    const currentRoundId;
+    const 
+    
+    function joinGame(name) {	
+	// 1. send deposit to smart-contract with name
+
+	// subscribe for smart-contrct game event
+	function _gameStartedCallback() {
+	    // 2. get peerIds from the smart-contract
+	    
+	    // 3. establish the connection with each peerId and subscribe for events
+
+	    function _getPeerRandomNumberCallback() {
+		
+		// save result
+		// if all peers have provided a number:
+		//  calculate result and define the winner
+		//  
+	    }	   
+	}		
+    }
+
+
+    function runRound() {	
+	// generate a large random number
+	
+	// Hash the number (and a nonce (block hash that started the game))
+	
+	// push round number hashed random number with other players 
+    }
+
+    
+
+    function claimPayout() {
+	// claim payout from the smart contract	
+    }
+
+    
+}
+
+
+export default new GameService();
---
+++ test/game-logic.js
@@ -0,0 +1,51 @@
+var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+
+contract('SimpleStorage', (accounts) => {
+
+    describe("game", () =>  {
+	it("should has current round number", () => {
+	    
+	});
+    });
+
+    describe("round", () => {
+	describe("Waiting hashed and signed random numbers", () => {
+	    it("client should be able to generate large random number", () => {
+		
+	    });
+	    
+	    it("client should be able to hash large number with a nonce", () => {
+	    
+	    });
+	    
+	    it("client should be able to sign hashed large number", () => {
+		
+	    });
+	    
+	    it("client should be able to share hashed large number", () => {
+		
+	    });
+	    
+	    describe("NOT last random number submitted", () => {
+		
+		it("round state should be pending randomness", () => {
+		    
+		});
+		
+	    });
+	    
+	    describe("the last player submits random number", () => {
+		it("round state should be pending reva", () => {
+		    
+		});
+		
+	    });
+	    
+	
+	});
+	
+	describe("Waiting random numbers", () => {
+	    
+	});
+    });    
+});
